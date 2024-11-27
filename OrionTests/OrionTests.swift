7927b40ee77a420c11a79b46e42af0234348c4ca
Ali Hamza Azam <114861246+AliHamzaAzam@users.noreply.github.com>
2024-11-27 14:00:47 +0500

    Initial Commit

---
+++ OrionTests/OrionTests.swift
@@ -0,0 +1,16 @@
+//
+//  OrionTests.swift
+//  OrionTests
+//
+//  Created by Ali Hamza Azam on 27/11/2024.
+//
+
+import Testing
+
+struct OrionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
