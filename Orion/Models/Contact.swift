a5512aa3abf65553d862fb475af605493e5973ab
Ali Hamza Azam <114861246+AliHamzaAzam@users.noreply.github.com>
2024-12-03 22:32:28 +0500

    Work in progress group chat implementation

---
+++ Orion/Models/Contact.swift
@@ -0,0 +1,16 @@
+//
+//  Contact.swift
+//  Orion
+//
+//  Created by Ali Hamza Azam on 01/12/2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct Contact: Identifiable {
+    let id: UUID
+    let name: String
+    let profileColor: Color
+    let initials: String
+}
--- Orion/Models/Group.swift
+++ Orion/Models/Group.swift
@@ -2,14 +2,7 @@
 //  Group.swift
 //  Orion
 //
-//  Created by Ali Hamza Azam on 30/11/2024.
+//  Created by Ali Hamza Azam on 03/12/2024.
 //
 
 import Foundation
-
-struct Group: Identifiable {
-    let id: UUID
-    let name: String
-    var members: [UUID]
-}
-
